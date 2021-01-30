5c17b7fae910c4d7881b2ee35d4e436f88514e28
moayadnajd <moayad.najd@gmail.com>
2021-01-30 22:09:27 +0200

    update to clarisa database

--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -2,15 +2,16 @@ require('@tensorflow/tfjs');
 import * as tf from '@tensorflow/tfjs-node';
 import * as use from "@tensorflow-models/universal-sentence-encoder"
 import * as path from 'path'
-import { Injectable, Logger } from '@nestjs/common';
-import * as clarisa from './clarisa.json'
+import { HttpService, Injectable, Logger } from '@nestjs/common';
+import { map } from 'rxjs/operators';
 @Injectable()
 export class AI {
     private readonly logger = new Logger(AI.name);
     model
+    clarisa
     naturalmodel
     traningModel: tf.Sequential
-    constructor() {
+    constructor(private httpService: HttpService) {
 
         this.init()
     }
@@ -19,6 +20,7 @@ export class AI {
         this.model = await tf.loadLayersModel('file://' + path.resolve(__dirname, 'wos_trained_model/model.json'));
         this.naturalmodel = await use.load()
         this.logger.log("Trained Model Loaded")
+        this.clarisa = await this.httpService.get('https://clarisa.cgiar.org/api/institutions', { auth: { username: 'gldc.data', password: '7823282' } }).pipe(map((d: any) => d.data)).toPromise()
     }
     calculatePercent(percent) {
         return Math.round(percent * 100);
@@ -26,7 +28,7 @@ export class AI {
     async makePrediction(value) {
         const todoEmbedding = await this.naturalmodel.embed(value.toLowerCase())
         let results: any = this.model.predict(todoEmbedding)
-        return { value: clarisa[results.argMax(1).dataSync()[0]], confidant: Math.max(...results.dataSync().map(d => d)) }
+        return { value: this.clarisa[results.argMax(1).dataSync()[0]], confidant: Math.max(...results.dataSync().map(d => d)) }
     }
 
 }
