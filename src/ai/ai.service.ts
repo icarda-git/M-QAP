a8f57412a74597bacdeab9c64b6817e289c8a6bf
moayadnajd <moayad.najd@gmail.com>
2022-10-05 12:48:13 +0300

    orcid and Pridiction

--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -2,9 +2,15 @@ require('@tensorflow/tfjs');
 import * as tf from '@tensorflow/tfjs-node';
 import * as use from '@tensorflow-models/universal-sentence-encoder';
 import * as path from 'path';
-import { HttpService, Injectable, Logger } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpService,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { catchError, map } from 'rxjs/operators';
-import * as fs from "fs";
+import * as fs from 'fs';
 @Injectable()
 export class AI {
   private readonly logger = new Logger(AI.name);
@@ -21,44 +27,56 @@ export class AI {
       'file://' + path.resolve(__dirname, 'wos_trained_model/model.json'),
     );
     this.naturalmodel = await use.load();
-    let rawdata:any = fs.readFileSync(path.resolve(__dirname, 'wos_trained_model/clarisa_data.json'));
-    this.clarisa = JSON.parse(rawdata);;
+    let rawdata: any = fs.readFileSync(
+      path.resolve(__dirname, 'wos_trained_model/clarisa_data.json'),
+    );
+    this.clarisa = JSON.parse(rawdata);
     this.logger.log('Trained Model Loaded');
   }
   calculatePercent(percent) {
     return Math.round(percent * 100);
   }
   async makePrediction(value, doi) {
-    const todoEmbedding = await this.naturalmodel.embed(value.toLowerCase());
-    const results: any = this.model.predict(todoEmbedding);
-    const clarisa_index = this.clarisa[results.argMax(1).dataSync()[0]];
-    if (clarisa_index)
-      this.httpService
-        .post(
-          process.env.MEL_API + 'prediction/external',
-          {
-            wos_name: value,
-            type: 'clarisa',
-            confident: Math.round(
-              Math.max(...results.dataSync().map((d) => d)) * 100,
-            ),
-            doi: doi,
-            clarisa_id: clarisa_index.code,
-          },
-          { headers: { authorization: process.env.MEL_API_KEY } },
-        )
-        .pipe(
-          map((d: any) => d.data),
-          catchError((e) => {
-            this.logger.log('MEL API is not connected');
-            this.logger.error(e);
-            return [null];
-          }),
-        )
-        .toPromise();
-    return {
-      value: clarisa_index ? clarisa_index : null,
-      confidant: Math.max(...results.dataSync().map((d) => d)),
-    };
+    try {
+      const todoEmbedding = await this.naturalmodel.embed(value.toLowerCase());
+      const results: any = this.model.predict(todoEmbedding);
+      const clarisa_index = this.clarisa[results.argMax(1).dataSync()[0]];
+      if (clarisa_index)
+        this.httpService
+          .post(
+            process.env.MEL_API + 'prediction/external',
+            {
+              wos_name: value,
+              type: 'clarisa',
+              confident: Math.round(
+                Math.max(...results.dataSync().map((d) => d)) * 100,
+              ),
+              doi: doi,
+              clarisa_id: clarisa_index.code,
+            },
+            { headers: { authorization: process.env.MEL_API_KEY } },
+          )
+          .pipe(
+            map((d: any) => d.data),
+            catchError((e) => {
+              this.logger.log('MEL API is not connected');
+              this.logger.error(e);
+              return [null];
+            }),
+          )
+          .toPromise();
+      return {
+        value: clarisa_index ? clarisa_index : null,
+        confidant: this.calculatePercent(
+          Math.max(...results.dataSync().map((d) => d)),
+        ),
+      };
+    } catch (e) {
+      if (e.message && (e.message as string).indexOf(`reading 'embed'`) >= 0)
+        throw new BadRequestException(
+          'please try again later AI is loading data',
+        );
+      else throw new InternalServerErrorException(e);
+    }
   }
 }
--- src/handle/handle.service.ts
+++ src/handle/handle.service.ts
@@ -1,5 +1,7 @@
 import { HttpService, Injectable } from '@nestjs/common';
+import { keep } from '@tensorflow/tfjs';
 import { map } from 'rxjs/operators';
+import { AI } from 'src/ai/ai.service';
 import { DoiService } from 'src/doi/doi.service';
 import { FormatSearvice } from './formater.service';
 import * as schema from './schema.json';
@@ -9,17 +11,83 @@ export class HandleService {
     private http: HttpService,
     private formatService: FormatSearvice,
     private doi: DoiService,
+    private ai: AI,
   ) {}
 
+  async toClarisa(Items, handle) {
+    if (!Array.isArray(Items)) Items = [Items];
+    let result = [];
+    for (let i in Items) {
+      result.push({
+        name: Items[i],
+        prediction: await this.ai.makePrediction(Items[i], handle),
+      });
+    }
+    return result;
+  }
+
   async getInfoByHandle(handle) {
-    console.log('getInfoByHandle', handle);
     const results = await Promise.all([
       this.getDpsace(handle),
       this.getAltmetricByHandle(handle),
     ]);
     let data = results[0];
+    if (data.Keywords)
+      data['agrovoc_keywords'] = await this.getAgrovocKeywords(data.Keywords);
+
+    if (data.ORCID) data['ORCID_Data'] = await this.getORCID(data.ORCID);
+
+    if (data.Affiliation)
+      data.Affiliation = await this.toClarisa(data.Affiliation, handle);
+
+    if (data['Funding source'])
+      data['Funding source'] = await this.toClarisa(
+        data['Funding source'],
+        handle,
+      );
+
     data['handle_altmetric'] = results[1];
     return data;
+  }
+
+  async getAgrovocKeywords(Keywords: [string]) {
+    let keywords_agro = [];
+    Keywords.forEach((keyw) => {
+      keywords_agro.push(
+        this.http
+          .get(
+            `https://agrovoc.uniroma2.it/agrovoc/rest/v1/agrovoc/search?query=${keyw}`,
+          )
+          .pipe(
+            map((d) => {
+              return { keyword: keyw, is_agrovoc: d.data.results.length > 0 };
+            }),
+          )
+          .toPromise(),
+      );
+    });
+    const results = await Promise.all(keywords_agro);
+    return {
+      keywords: results.filter((d) => d.is_agrovoc).map((d) => d.keyword),
+      results,
+    };
+  }
+
+  async getORCID(orcids) {
+    let tohit = [];
+    orcids.forEach((orcid) => {
+      orcid = orcid.split(': ')[1];
+      tohit.push(
+        this.http
+          .get(`https://orcid.org/${orcid}`, {
+            headers: { Accept: 'application/json' },
+          })
+          .pipe(map((d) => d.data))
+          .toPromise()
+          .catch((e) => null),
+      );
+    });
+    return await Promise.all(tohit);
   }
 
   async getDpsace(handle) {
