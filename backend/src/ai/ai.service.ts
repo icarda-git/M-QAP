4d8d6bd392cd3961672e994f01bb65f532649aa4
moayadnajd <moayad.najd@gmail.com>
2023-11-13 16:27:15 +0300

    #10

--- backend/src/ai/ai.service.ts
+++ backend/src/ai/ai.service.ts
@@ -13,6 +13,7 @@ import { catchError, map } from 'rxjs/operators';
 import * as fs from 'fs';
 import { firstValueFrom } from 'rxjs';
 import { PredictionsService } from 'src/predictions/predictions.service';
+import { TrainningCycleService } from 'src/trainning-cycle/trainning-cycle.service';
 @Injectable()
 export class AI {
   private readonly logger = new Logger(AI.name);
@@ -22,21 +23,32 @@ export class AI {
   traningModel: tf.Sequential;
   constructor(
     private httpService: HttpService,
-    private predictionsService: PredictionsService
+    private predictionsService: PredictionsService,
+    private trainning_cycle: TrainningCycleService,
   ) {
     this.init();
   }
   async init() {
-    this.logger.log('Start Loading Trained Model ' + process.env.PRODUCTION);
-    this.model = await tf.loadLayersModel(
-      'file://' + path.resolve(__dirname, 'wos_trained_model/model.json'),
-    );
-    this.naturalmodel = await use.load();
-    let rawdata: any = fs.readFileSync(
-      path.resolve(__dirname, 'wos_trained_model/clarisa_data.json'),
-    );
-    this.clarisa = JSON.parse(rawdata);
-    this.logger.log('Trained Model Loaded');
+      const active_cycle = await this.trainning_cycle.findLatestOne();
+      const training_folder_path = path.join(
+        process.cwd(),
+        'uploads/training-data/' + active_cycle.id,
+      );
+      this.logger.log(
+        'Start Loading Trained Model ' +
+          process.env.PRODUCTION +
+          ' ' +
+          training_folder_path,
+      );
+      this.model = await tf.loadLayersModel(
+        'file://' + training_folder_path + '/model.json',
+      );
+      this.naturalmodel = await use.load();
+      let rawdata: any = fs.readFileSync(
+        training_folder_path + '/clarisa_data.json',
+      );
+      this.clarisa = JSON.parse(rawdata);
+      this.logger.log('Trained Model Loaded');
   }
   calculatePercent(percent) {
     return Math.round(percent * 100);
@@ -76,7 +88,7 @@ export class AI {
         Math.max(...results.dataSync().map((d) => d)),
       );
       const clarisa_id = clarisa_index ? clarisa_index.code : null;
-      
+
       this.predictionsService.create({ confidant, clarisa_id, text: value });
       return {
         value: clarisa_index ? clarisa_index : null,
--- backend/src/trainning-cycle/trainning-cycle.controller.ts
+++ backend/src/trainning-cycle/trainning-cycle.controller.ts
@@ -1,40 +1,65 @@
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    Patch,
-    Post,
-  } from '@nestjs/common';
-  import { TrainningCycleService } from './trainning-cycle.service';
-  
-  @Controller('trainning-cycle')
-  export class TrainningCycleController {
-    constructor(private trainningCycleService: TrainningCycleService) {}
-    @Post()
-    create(@Body() createUserDto: any) {
-      return this.trainningCycleService.create(createUserDto);
-    }
-  
-    @Get('')
-    findAll() {
-      return this.trainningCycleService.findAll();
-    }
-  
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-      return this.trainningCycleService.findOne(+id);
-    }
-  
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateUserDto: any) {
-      return this.trainningCycleService.update(+id, updateUserDto);
-    }
-  
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-      return this.trainningCycleService.remove(+id);
-    }
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { TrainningCycleService } from './trainning-cycle.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as fs from 'fs';
+import * as path from 'path';
+@Controller('trainning-cycle')
+export class TrainningCycleController {
+  constructor(private trainningCycleService: TrainningCycleService) {}
+  @Post()
+  create(@Body() createUserDto: any) {
+    return this.trainningCycleService.create(createUserDto);
   }
-  
+
+  @Post(':id')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Param('id') id,
+    @Body('type') type,
+  ) {
+    const training_folder_path = path.join(
+      process.cwd(),
+      'uploads/training-data/' + id,
+    );
+    fs.mkdirSync(training_folder_path, {
+      recursive: true,
+    });
+    fs.writeFileSync(
+      path.join(training_folder_path, '/' + file.originalname),
+      file.buffer,
+    );
+
+    return file;
+  }
+
+  @Get('')
+  findAll() {
+    return this.trainningCycleService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.trainningCycleService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: any) {
+    return this.trainningCycleService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.trainningCycleService.remove(+id);
+  }
+}
